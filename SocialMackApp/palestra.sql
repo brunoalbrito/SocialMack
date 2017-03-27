40d1f164847ca6b8546787f9b9a130db30dafdae
brunoalbrito <brunoalbuquerquebrito@outlook.com>
2017-03-27 18:22:04 -0300

    Clear the folder

--- SocialMackApp/palestra.sql
+++
@@ -1,5 +0,0 @@
-create table palestra(id_palestra bigint not null primary key generated
-    always as identity (start with 1, increment by 1), 
-    tema varchar(50) not null,
-    descricao varchar(50) not null,
-    palestrante varchar(50) not null); 
